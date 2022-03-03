44a1862857f70a9db8f59be7fe6f3fab3163a475
Rohit Bhardwaj <rohitbhardwajindia@gmail.com>
2022-03-03 16:58:26 +0530

    error handler added

--- hello/hello.go
+++ hello/hello.go
@@ -3,10 +3,20 @@ package main
 import (
 	"fmt"
 
+	"log"
+
 	"example.com/greetings"
 )
 
 func main() {
-	message := greetings.Hello("Rohit")
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	message, err = greetings.Hello("")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	
 	fmt.Println(message)
 }
